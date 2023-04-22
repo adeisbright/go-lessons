cff2abd0289546645684fe991dc2262441c0e727
Adeleke Bright <bigjaratech@gmail.com>
2023-04-22 19:02:16 +0100

    Use Bcrypt

---
+++ go.mod
@@ -0,0 +1,9 @@
+module mypackages
+
+go 1.17
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.8.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Sex struct {
@@ -18,6 +24,17 @@ func checkError(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckHashedPassword(hashedPassword string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	fmt.Println(err)
+	return err == nil
 }
 
 func main() {
@@ -67,4 +84,71 @@ func main() {
 	os.Rename(OldName , newName) Renames a file from old to new
 	os.Remove(fileName) Removes a file
 	*/
+
+	err = godotenv.Load(".env")
+	checkError(err)
+	fmt.Println(os.Getenv("APP_NAME"))
+
+	//Working with JWT
+	var (
+		key []byte
+		t   *jwt.Token
+		s   string
+	)
+
+	type MyClaims struct {
+		Foo string `json:"foo"`
+		jwt.RegisteredClaims
+	}
+
+	claims := MyClaims{
+		"bar",
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(10 * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "test",
+			Subject:   "somebody",
+			ID:        "1",
+			Audience:  []string{"somebody_else"},
+		},
+	}
+
+	key = []byte("Bigajra")
+	// t = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	// 	"iss": "Bigjara",
+	// 	"foo": "bar",
+	// })\
+	t = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	s, _ = t.SignedString(key)
+
+	fmt.Println(s)
+
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpc3MiOiJ0ZXN0Iiwic3ViIjoic29tZWJvZHkiLCJhdWQiOlsic29tZWJvZHlfZWxzZSJdLCJleHAiOjE2ODIxMzg0ODYsIm5iZiI6MTY4MjEzNzg4NiwiaWF0IjoxNjgyMTM3ODg2LCJqdGkiOiIxIn0.BwV87QfmjKQIYG8YEaU9AAvAhsyMeD4TSfIL_f4-5n8"
+	token, _ := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("All your base"), nil
+	})
+
+	if token.Valid {
+		fmt.Println("You look nice today")
+	} else if errors.Is(err, jwt.ErrTokenMalformed) {
+		fmt.Println("That's not even a token")
+	} else if errors.Is(err, jwt.ErrTokenSignatureInvalid) {
+		// Invalid signature
+		fmt.Println("Invalid signature")
+	} else if errors.Is(err, jwt.ErrTokenExpired) || errors.Is(err, jwt.ErrTokenNotValidYet) {
+		// Token is either expired or not active yet
+		fmt.Println("Timing is everything")
+	} else {
+		fmt.Println("Couldn't handle this token:", err)
+	}
+
+	password := "12345678"
+	hashedPassword, err := HashPassword(password)
+	checkError(err)
+	fmt.Printf("%v , %v , \n", "Your hashed password is", hashedPassword)
+	//Check if the password is correct
+	isCorrectPassword := CheckHashedPassword(hashedPassword, password)
+	fmt.Println(isCorrectPassword)
+
 }
